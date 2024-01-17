ab1266168a00d1910cd52b37cef7305335bb095f
Geffrey van der Bos <geffrey.bos@gmail.com>
2024-01-17 10:26:51 +0100

    performance improvements like lazy loading and smaller images

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,6 @@ const md = new markdownIt();
 const { formatDistanceToNow } = require('date-fns');
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
-
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/stylesheets/*.css");
     eleventyConfig.addPassthroughCopy("./src/scripts/*.js");
