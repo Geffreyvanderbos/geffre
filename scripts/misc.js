a71aa82edfa4105eb15b0d203b7cb740cc1eb044
Geffreyvanderbos <Geffreyvanderbos@users.noreply.github.com>
2024-03-31 15:43:58 +0000

    deploy: 9d20ffc87dda834f1e5d9c9f3521152fdc66464e

--- scripts/misc.js
+++ scripts/misc.js
@@ -26,21 +26,24 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-//  Like button sparks!
+// Like button sparks!
 document.addEventListener('DOMContentLoaded', function() {
     const likeButton = document.querySelector('.tinylytics_kudos');
 
     likeButton.addEventListener('click', function() {
+        // Check if the button is disabled
+        if (this.disabled) {
+            return; // Do nothing if the button is disabled
+        }
+
         // Generate multiple sparks
-        for (let i = 0; i < 20; i++) { // Change 10 to the number of sparks you want
+        for (let i = 0; i < 20; i++) { // Increase the number of sparks if desired
             const spark = document.createElement('div');
             spark.style.position = 'absolute';
-            spark.style.width = '1px'; // Slightly larger spark
+            spark.style.width = '1px'; // Spark size
             spark.style.height = '1px';
             spark.style.backgroundColor = 'black';
-            // spark.style.left = `${Math.random() * 5}%`;
-            // spark.style.top = `${Math.random() * 5}%`;
-            spark.style.left = `44px`;
+            spark.style.left = `44px`; // Starting position of the spark
             spark.style.top = `18px`;
 
             // Add the spark to the container
@@ -48,16 +51,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
             // Animate the spark
             const plusOrMinus = Math.random() < 0.5 ? -1 : 1;
-            const animateX = plusOrMinus * Math.random() * 30; // Adjust for larger spread
+            const animateX = plusOrMinus * Math.random() * 30; // Spread of the spark
             const animateY = plusOrMinus * Math.random() * 30;
 
             spark.animate([
                 // Keyframes
-                { transform: 'translate(0, 0)', opacity: .7 },
+                { transform: 'translate(0, 0)', opacity: 0.7 },
                 { transform: `translate(${animateX}px, ${animateY}px)`, opacity: 0 }
             ], {
                 // Timing options
-                duration: Math.random() * 300 + 600, // Randomize duration for each spark
+                duration: Math.random() * 300 + 600, // Duration of the spark animation
                 easing: 'ease-out',
                 fill: 'forwards'
             });
